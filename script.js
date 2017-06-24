c150716c16e29b22f198739c04f95129d7bdf181
Brandon Bledsoe <brbledsoe.m@gmail.com>
2017-06-24 21:02:36 +0300

    made code more modular via adding closure in script js

--- script.js
+++ script.js
@@ -1,77 +1,97 @@
-var canvas = [
-[0, 0, 0, 0],
-[0, 0, 0, 0],
-[0, 0, 0, 0],
-[0, 0, 0, 0]
-];
+function worm_game () {
 
-//first item row — second column
-var wormPoint = [0, 0];
+ var canvas = [
+   [0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0],
+ ];
 
-function renderCanvas () {
+ //x y coordinate of worm head
+ var wormHead = [0, 0];
 
-  //empty the current canvas div
-  $('.canvas').empty();
+ function renderCanvas () {
 
-  for(var i = 0; i < canvas.length; i++){
-    $('.canvas').append('<section class="canvas_row_'+ i +'"></section>');
-    for(var j = 0; j < canvas[i].length; j++){
-      if(i === wormPoint[0] && j === wormPoint[1]){
-        $('.canvas_row_' + i).append('<div class="canvas_col red"></div>')
-      }else{
-        $('.canvas_row_' + i).append('<div class="canvas_col"></div>')
-      }
-    }
-  }
+   //empty the current canvas div
+   $('.canvas').empty();
+
+   //append rows
+   for(var i = 0; i < canvas.length; i++){
+     $('.canvas').append('<section class="row canvas_row_'+ i +'"></section>');
+
+     //append columns within rows
+     for(var j = 0; j < canvas[i].length; j++){
+       if(i === wormHead[0] && j === wormHead[1]){
+         $('.canvas_row_' + i).append('<div class="canvas_col red"></div>')
+       }else{
+         $('.canvas_row_' + i).append('<div class="canvas_col"></div>')
+       }
+     }
+   }
+ }
+
+ function moveUp(){
+   wormHead.splice(0, 1, (wormHead[0] - 1));
+   renderCanvas()
+   console.log(canvas)  
+ }
+
+ function moveRight(){
+   wormHead.splice(1, 1, (wormHead[1] + 1));
+   renderCanvas()  
+   console.log(canvas)
+ }
+
+ function moveDown(){
+   wormHead.splice(0, 1, (wormHead[0] + 1));
+   renderCanvas()
+   console.log(canvas)  
+ }
+
+ function moveLeft(){
+   wormHead.splice(1, 1, (wormHead[1] - 1));
+   renderCanvas()
+   console.log(canvas)  
+ }
+
+ return {
+  renderCanvas: renderCanvas,
+  moveUp: moveUp,
+  moveRight: moveRight,
+  moveDown: moveDown,
+  moveLeft: moveLeft
+ }
 }
 
-//fruits.splice(2, 0, "Lemon", "Kiwi");
-function moveUp(){
-  wormPoint.splice(0, 1, (wormPoint[0] - 1));
-  renderCanvas()
-  console.log(canvas)  
-}
-
-function moveRight(){
-  wormPoint.splice(1, 1, (wormPoint[1] + 1));
-  renderCanvas()  
-  console.log(canvas)
-}
-
-function moveDown(){
-  wormPoint.splice(0, 1, (wormPoint[0] + 1));
-  renderCanvas()
-  console.log(canvas)  
-}
-
-function moveLeft(){
-  wormPoint.splice(1, 1, (wormPoint[1] - 1));
-  renderCanvas()
-  console.log(canvas)  
-}
+//new game
+var app = worm_game();
+app.renderCanvas()
 
 $(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-        moveLeft();
-        break;
+  switch(e.which) {
+   case 37: // left
+   app.moveLeft();
+   break;
 
-        case 38: // up
-        moveUp();
-        break;
+   case 38: // up
+   app.moveUp();
+   break;
 
-        case 39: // right
-        moveRight();
-        break;
+   case 39: // right
+   app.moveRight();
+   break;
 
-        case 40: // down
-        moveDown();
-        break;
+   case 40: // down
+   app.moveDown();
+   break;
 
-        default: return; // exit this handler for other keys
-    }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
+   default: return; // exit this handler for other keys
+  }
+
+  e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
-renderCanvas();
+alert('hey')
 
