e876772a1e676563c276bb65d8b3a47c71a6d61e
Brandon Bledsoe <brbledsoe.m@gmail.com>
2017-07-16 15:45:50 +0300

    used a different 'faster' strategy within move functions...testing whether its faster than last version's move functions

--- script.js
+++ script.js
@@ -1,129 +1,105 @@
-var canvasRowLength = 40;
-var canvasColLength = 59;
+var canvasRows = 40;
+var canvasTilesPerRow = 59;
 var snakeHeadX = 0; //snakeHead x coordinate
 var snakeHeadY = 0; //snakeHead y coordinate
 var direction = 'right';
-//length of snake tail
 var tail = 0;
 
 function renderCanvas() {
     //empty the current canvas div
     $('.canvas').empty();
 
-    //width of columns is length of array divided by 100% width
-    var col_width = 100 / canvasColLength;
+    var tileWidth = 'width:' + 100/canvasTilesPerRow + '%; ';
+    var tileHeight = 'height:' + 100/canvasTilesPerRow + '%;';
+    var tileDimensions = tileWidth + tileHeight;
 
-    //append rows
-    for (var i = 0; i < canvasRowLength; i++) {
-        var canvasRow = '<section class="canvas_row_' + i + '"></section>';
-        $('.canvas').append(canvasRow);
+    for (var i = 0; i < canvasRows * canvasTilesPerRow; i++) {
+        var snakeHeadClass = (i === 0) ? 'red' : '';
 
-        //append columns
-        for (var j = 0; j < canvasColLength; j++) {
-            //if i & j on x & y coordinates of snakeHead, add snakeHeadClass, else nothing
-            var snakeHeadClass = (i === snakeHeadY && j === snakeHeadX) ? 'red' : '';
-
-            //canvas col to add
-            var canvasCol = '<div class="canvas_col canvas_col_' + j + '" style="width:' + col_width + '%;">' +
-                '<div class="canvas_col_tile ' + snakeHeadClass +'"></div>' +
-                '</div>'
-
-            //append canvas col to current canvasRow
-            $('.canvas_row_' + i).append(canvasCol);
-
-        }
+        $('.canvas').append('<div class="canvas_tile ' + snakeHeadClass + '" id="' + i + '" style="' + tileDimensions + '"></div>');
     }
 }
 
 function moveUp() {
-    //remove class red from currentSnake head
-    $(".red").removeClass('red');
+  //get current snakeHead div's id
+  var $currentDiv = $("div.red")["0"].id;
+  //create next snakeHead div's id
+  var $nextDiv = "#" + (parseInt($currentDiv) - canvasTilesPerRow);
 
-    //don't let snake go out of bounds
-    //ie change snakeHead x coord so long as we don't go lower than 0 ( no - 1)
-    snakeHeadY = Math.max(snakeHeadY - 1, 0);
-
-    //new row
-    var nextSnakeHeadRow = '.canvas_row_' + snakeHeadY;
-    //new col
-    var nextSnakeHeadCol = ' .canvas_col_' + snakeHeadX + ' .canvas_col_tile';
-
-    //add class red to new Snake head
-    $(nextSnakeHeadRow + nextSnakeHeadCol).addClass('red');
-
-    //change direction of Snake movement
-    direction = 'up';
+  //remove current snakeHead div's red class
+  $('#' + $currentDiv).removeClass('red');
+  //add red class to next snakeHead div
+  $($nextDiv).addClass('red');
+  
+  //change snakeHead's moving direction
+  direction = 'up';
 }
 
 function moveRight() {
-    //remove class red from currentSnake head
-    $(".red").removeClass('red');
+    //get current snakeHead div's id
+    var $currentDiv = $("div.red")["0"].id;
+    //create next snakeHead div's id
+    var $nextDiv = "#" + (parseInt($currentDiv) + 1);
 
-    //change snakeHead y coord so long as it doesn't exceed the length of a canvas array
-    snakeHeadX = Math.min(snakeHeadX + 1, canvasColLength - 1)
+    //remove current snakeHead div's red class
+    $('#' + $currentDiv).removeClass('red');
+    //add red class to next snakeHead div
+    $($nextDiv).addClass('red');
 
-    //new row
-    var nextSnakeHeadRow = '.canvas_row_' + snakeHeadY;
-    //new col
-    var nextSnakeHeadCol = ' .canvas_col_' + snakeHeadX + ' .canvas_col_tile';
-    $(nextSnakeHeadRow + nextSnakeHeadCol).addClass('red');
-
+    //change snakeHead's moving direction
     direction = 'right';
 }
 
 function moveDown() {
-    //remove class red from currentSnake head
-    $(".red").removeClass('red');
+  //get current snakeHead div's id
+  var $currentDiv = $("div.red")["0"].id;
+  //create next snakeHead div's id
+  var $nextDiv = "#" + (parseInt($currentDiv) + canvasTilesPerRow);
 
-    //change snakeHead x coord so long as it doesn't exceed the length of canvas
-    snakeHeadY = Math.min(snakeHeadY + 1, canvasRowLength - 1)
-
-    //new row
-    var nextSnakeHeadRow = '.canvas_row_' + snakeHeadY
-    //new col
-    var nextSnakeHeadCol = ' .canvas_col_' + snakeHeadX + ' .canvas_col_tile';
-    //add the class
-    $(nextSnakeHeadRow + nextSnakeHeadCol).addClass('red');
-
-    direction = 'down';
+  //remove current snakeHead div's red class
+  $('#' + $currentDiv).removeClass('red');
+  //add red class to next snakeHead div
+  $($nextDiv).addClass('red');
+  
+  //change snakeHead's moving direction
+  direction = 'down';
 }
 
 function moveLeft() {
-    //remove class red from currentSnake head
-    $(".red").removeClass('red');
+    //get current snakeHead div's id
+    var $currentDiv = $("div.red")["0"].id;
+    //create next snakeHead div's id
+    var $nextDiv = "#" + (parseInt($currentDiv) - 1);
 
-    //change snakeHead x coord so long as we don't go lower than 0 ( no -1 )
-    snakeHeadX = Math.max(snakeHeadX - 1, 0)
-
-    //new row
-    var nextSnakeHeadRow = '.canvas_row_' + snakeHeadY;
-    //new col
-    var nextSnakeHeadCol = ' .canvas_col_' + snakeHeadX + ' .canvas_col_tile';
-    $(nextSnakeHeadRow + nextSnakeHeadCol).addClass('red');
-
+    //remove current snakeHead div's red class
+    $('#' + $currentDiv).removeClass('red');
+    //add red class to next snakeHead div
+    $($nextDiv).addClass('red');
+    
+    //change snakeHead's moving direction
     direction = 'left';
 }
 
 //new game
 renderCanvas()
 
-setInterval(function() {
-    if (direction === 'up') {
-        moveUp();
-    }
+// setInterval(function() {
+//     if (direction === 'up') {
+//         moveUp();
+//     }
 
-    if (direction === 'right') {
-        moveRight();
-    }
+//     if (direction === 'right') {
+//         moveRight();
+//     }
 
-    if (direction === 'down') {
-        moveDown();
-    }
+//     if (direction === 'down') {
+//         moveDown();
+//     }
 
-    if (direction === 'left') {
-        moveLeft();
-    }
-}, 1000)
+//     if (direction === 'left') {
+//         moveLeft();
+//     }
+// }, 1000)
 
 $(document).keydown(function(e) {
     switch (e.which) {
